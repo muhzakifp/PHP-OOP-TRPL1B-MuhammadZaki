8145b01c2d8dc7da5ca3c7a38dd10bc39e6b62b9
jaki <mzakifadhila@gmail.com>
2026-06-01 20:51:07 +0700

    modif_interface

--- interfaces.php
+++ interfaces.php
@@ -1,34 +1,38 @@
 <?php
-interface Animal {
-  public function fromFamily();
-  public function makeSound();
+interface Pembayaran{
+  function bayar($nominal);
 }
 
-class Cat implements Animal {
-  public function fromFamily() {
-    echo "From family: Felidae (Relatives: lions, tigers, jaguars, lynx, cougars, and cheetahs).<br>";
+class Transaksi implements Pembayaran{
+  public $barang, $harga;
+
+  function __construct($barang,$harga){
+    $this->barang = $barang;
+    $this->harga = $harga;
   }
-  public function makeSound() {
-    echo "Sound: Meow.";
+
+  function informasiBarang(){
+    echo "Barang yang dibeli ".$this->barang."<br>";
+    echo "Harga Barang ".$this->harga."<br>";
+  }
+
+  function bayar($nominal){
+    echo "Anda membayar dengan nominal Rp ".$nominal."<br>";
+
+    if($this->harga == $nominal){
+      echo "Pembayaran berhasil !";
+    }else if ($this->harga < $nominal){
+      $kembalian = $nominal - $this->harga;
+      echo "Uang anda lebih, dan sisa Rp".$kembalian;
+    }else{
+      $kurang = $this->harga - $nominal;
+      echo "Uang anda kurang Rp ".$kurang;
+    }
   }
 }
 
-class Dog implements Animal {
-  public function fromFamily() {
-    echo "From family: Canidae (Relatives: wolves, foxes, coyotes, jackals, and domestic dogs).<br>";
-  }
-  public function makeSound() {
-    echo "Sound: Woff.";
-  }
-}
+$transaksi = new Transaksi("Musang ",100000);
+$transaksi->informasiBarang();
+$transaksi->bayar(100000);
 
-
-$cat = new Cat();
-$cat->fromFamily();
-$cat->makeSound();
-
-echo "<h3>Dogs</h3>";
-$dog = new Dog();
-$dog->fromFamily();
-$dog->makeSound();
 ?>
